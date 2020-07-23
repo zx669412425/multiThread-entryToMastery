920ead3b91cfa9bf8308cb4049e2b3b828930f7b
ZhangXin_163 <zhangxin_0725@163.com>
2020-07-23 22:47:43 +0800

    add ch2 section3

---
+++ src/main/java/ch2/Test.java
@@ -0,0 +1,8 @@
+package ch2;
+
+public class Test {
+    public static void main(String[] args) {
+        Thread.currentThread().isInterrupted();
+        //Thread.currentThread().interrupted();
+    }
+}
